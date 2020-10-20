2aa6ff92f03fdaa3aaf6bfd193545ff6d14122d7
wen4752 <62042730+wen4752@users.noreply.github.com>
2020-10-20 15:11:25 +0800

    rak

--- register/coba.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$error = array("error"=>true,"username_err"=>null, "password_err"=>null, "confirm_password_err"=>null, "nama_err" =>null, "email_err"=>null);
-
-foreach ($error as $key => $value) {
-	if($key!="error")
-		print("key: ".$key." value: ".$value);
-}
- ?>
--- register/config.php
+++ register/config.php
@@ -1,5 +1,5 @@
 <?php 
-$con = mysqli_connect("localhost","root","","manpro");
+$con = mysqli_connect("localhost","root","","dbgudang");
 if($con===false)
 	die("Error: Could not connect. ".mysqli_connect_error());
 
