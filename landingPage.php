df63c5ae45d7cc2dd67015c66b6a47267a1a865d
nicho2605 <c14180046@john.petra.ac.id>
2020-10-30 16:42:59 +0700

    CSS
    css landing + home

--- landingPage.php
+++ landingPage.php
@@ -1,35 +1,36 @@
 <!DOCTYPE html>
     <head>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script type="text/javascript" src="http://livejs.com/live.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="landing.css">
     </head>
     <body>
-
-    <!-- card login -->
-    <div class="card" style="width:30%; margin:0 auto; ">
-        <div class="card-body">
-
-            <!-- Input Username -->
-            <div class="input-group mb-3" style="margin-bottom:20px;">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Username</span>
+        <!-- card login -->
+        <div class="card" >
+            <div class="card-body">
+                <div class="title">
+                    <h2>Login</h2>
                 </div>
-                    <input type="text" id="username" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" required>
-            </div>
-
-            <!-- Input Password -->
-            <div class="input-group mb-3" style="margin-bottom:20px;">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Password</span>
+                <!-- Input Username -->
+                <div class="input-group mb-3">
+                    <input type="text" id="username" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Username" required>
                 </div>
-                    <input type="password" id="password" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" required password>
-            </div>
 
-            <div><button type="button" class="btn btn-primary" id="loginButton" style="width:100%; margin:0 auto;">Login</button></div>
+                <!-- Input Password -->
+                <div class="input-group mb-3">
+                    <input type="password" id="password" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="Password" required password>
+                </div>
+                <div class="elements">
+                    <a id="clickable" onclick="showhide()" >Show Password</a>
+                </div>
+                <div>
+                    <button type="button" class="btn btn-primary" id="loginButton" style="width:100%; margin:0 auto; background-color :#513826 ">Login</button>
+                </div>
+            </div>
         </div>
-    </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script type="text/javascript">
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript">
         $(document).ready(function(){
             $("#loginButton").click(function(){
                 var username = $("#username").val();
@@ -49,9 +50,17 @@
                     }
                 })
             });
-
         });
-
+        function showhide(){
+            var x = document.getElementById("password");
+            var y = document.getElementById("clickable");
+            if (x.type == "password") {
+                x.type = "text";
+                y.innerHTML = "Hide Password";
+            }else{
+                x.type = "password";
+                y.innerHTML = "Show Password";
+            }
+        }
     </script>
-    </body>
 </html>
